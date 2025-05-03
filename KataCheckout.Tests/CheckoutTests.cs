502533d599755665ce70ebd06ab1573a5e046e9f
sgr-0007 <sagarhs0007@gmail.com>
2025-05-03 23:09:23 +0100

    Add failing test for scanning special offer (3 for 130)

--- KataCheckout.Tests/CheckoutTests.cs
+++ KataCheckout.Tests/CheckoutTests.cs
@@ -10,7 +10,7 @@ namespace KataCheckout.Tests
         [Fact]
         public void WhenNoItemsScanned_TotalPriceIsZero()
         {
-            ICheckout checkout = new Checkout();
+            var checkout = new Checkout();
             var totalPrice = checkout.GetTotalPrice();
             Assert.Equal(0, totalPrice);
         }
@@ -19,7 +19,7 @@ namespace KataCheckout.Tests
         public void WhenSingleItemAScanned_TotalPriceIs50()
         {
             var pricingRules = new Dictionary<string, int> { { "A", 50 } };
-            ICheckout checkout = new Checkout(pricingRules);
+            var checkout = new Checkout(pricingRules);
             checkout.Scan("A");
             var totalPrice = checkout.GetTotalPrice();
             Assert.Equal(50, totalPrice);
@@ -30,12 +30,12 @@ namespace KataCheckout.Tests
         {
             var pricingRules = new Dictionary<string, int>
     {
-        { "A", 50 },
-        { "B", 30 },
-        { "C", 20 },
-        { "D", 15 }
+            { "A", 50 },
+            { "B", 30 },
+            { "C", 20 },
+            { "D", 15 }
     };
-            ICheckout checkout = new Checkout(pricingRules);
+            var checkout = new Checkout(pricingRules);
 
             checkout.Scan("A");
             checkout.Scan("B");
@@ -43,6 +43,18 @@ namespace KataCheckout.Tests
             var totalPrice = checkout.GetTotalPrice();
 
             Assert.Equal(100, totalPrice); // 50 + 30 + 20 = 100
+        }
+        [Fact]
+        public void WhenThreeItemsAScanned_SpecialPriceApplied()
+        {
+            var checkout = new Checkout();
+
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+            var totalPrice = checkout.GetTotalPrice();
+
+            Assert.Equal(130, totalPrice); // Special price: 3A for 130
         }
     }
 }
