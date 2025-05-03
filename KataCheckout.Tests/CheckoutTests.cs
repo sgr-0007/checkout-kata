6b0f7d5a1a36fc32c86e76adf8aaf47c223bf82e
sgr-0007 <sagarhs0007@gmail.com>
2025-05-03 14:59:13 +0100

    Add test for multiple items being scanned

--- KataCheckout.Tests/CheckoutTests.cs
+++ KataCheckout.Tests/CheckoutTests.cs
@@ -16,13 +16,33 @@ namespace KataCheckout.Tests
         }
 
         [Fact]
-public void WhenSingleItemAScanned_TotalPriceIs50()
-{
-    var pricingRules = new Dictionary<string, int> { { "A", 50 } };
-    ICheckout checkout = new Checkout(pricingRules);
-    checkout.Scan("A");
-    var totalPrice = checkout.GetTotalPrice();
-    Assert.Equal(50, totalPrice);
-}
+        public void WhenSingleItemAScanned_TotalPriceIs50()
+        {
+            var pricingRules = new Dictionary<string, int> { { "A", 50 } };
+            ICheckout checkout = new Checkout(pricingRules);
+            checkout.Scan("A");
+            var totalPrice = checkout.GetTotalPrice();
+            Assert.Equal(50, totalPrice);
+        }
+
+        [Fact]
+        public void WhenMultipleItemsScanned_TotalPriceIsSum()
+        {
+            var pricingRules = new Dictionary<string, int>
+            {
+                { "A", 50 },
+                { "B", 30 },
+                { "C", 20 },
+                { "D", 15 }
+            };
+            ICheckout checkout = new Checkout(pricingRules);
+
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("C");
+            var totalPrice = checkout.GetTotalPrice();
+
+            Assert.Equal(100, totalPrice); // 50 + 30 + 20 = 100
+        }
     }
 }
