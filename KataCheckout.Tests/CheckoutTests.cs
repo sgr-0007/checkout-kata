e9bc0d3dca3f8e92702bef8cfe1493998e83e77c
sgr-0007 <sagarhs0007@gmail.com>
2025-05-03 14:19:46 +0100

    Add failing test for scanning a single item

--- KataCheckout.Tests/CheckoutTests.cs
+++ KataCheckout.Tests/CheckoutTests.cs
@@ -14,5 +14,15 @@ namespace KataCheckout.Tests
             var totalPrice = checkout.GetTotalPrice();
             Assert.Equal(0, totalPrice);
         }
+
+        [Fact]
+public void WhenSingleItemAScanned_TotalPriceIs50()
+{
+    var pricingRules = new Dictionary<string, int> { { "A", 50 } };
+    ICheckout checkout = new Checkout(pricingRules);
+    checkout.Scan("A");
+    var totalPrice = checkout.GetTotalPrice();
+    Assert.Equal(50, totalPrice);
+}
     }
 }
--- KataCheckout/Services/Checkout.cs
+++ KataCheckout/Services/Checkout.cs
@@ -5,14 +5,32 @@ namespace KataCheckout.Services
 {
     public class Checkout : ICheckout
     {
+        private readonly Dictionary<string, int> _pricingRules;
+        private readonly Dictionary<string, int> _scannedItems;
+        
+        // Default constructor for when no items are scanne
+        public Checkout()
+        {
+            _pricingRules = new Dictionary<string, int>();
+            _scannedItems = new Dictionary<string, int>();
+        }
+        
         public void Scan(string item)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(item))
+                return;
+                
+            if (!_scannedItems.ContainsKey(item))
+            {
+                _scannedItems[item] = 0;
+            }
+            
+            _scannedItems[item]++;
         }
 
         public int GetTotalPrice()
         {
-            return 0;
+            return 0;   
         }
     }
 }
